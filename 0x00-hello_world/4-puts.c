714b6644d778a0d4168568718cbea2e0afbf537f
Milli-saved <million12.tenkir@gmail.com>
2021-06-17 12:18:09 -0700

    DOne

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * The function prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
