ecb8ff68ba1d2134da1ef20931bb84f1be3690ef
Milli-saved <million12.tenkir@gmail.com>
2021-06-17 12:26:18 -0700

    Done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * The function display a string on the screen
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
