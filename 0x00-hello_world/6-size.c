7cee6fc83f426dbe96a009d6aeac5dfc6d3e6746
Milli-saved <million12.tenkir@gmail.com>
2021-06-17 12:43:11 -0700

    Done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,9 @@
 /**
  * main - Entry point of code
  *
- * Return : 0(success)
+ * prints the size of various types on the computer it is compiled and run on.
+ *
+ * Return : 0
  */
 
 int main(void)
