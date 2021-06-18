c0a6ce2d49ada53055bcd1bb7a856b11c370083c
Milli-saved <million12.tenkir@gmail.com>
2021-06-18 10:14:17 -0700

    DOne

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,14 +15,12 @@ int main(){
 char ch;
 
 for (ch = 'a' ; ch <= 'z' ; ch++)
-{
 putchar(ch);
-}
+
 for (ch = 'A' ; ch <= 'Z' ; ch++)
-{
 putchar(ch);
-}
+
   
 putchar('\n');
-return 0;
+return (0);
 }
