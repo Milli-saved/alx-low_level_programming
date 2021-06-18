e52b6450fe78787c50bc64201dedea633f8b6723
Milli-saved <million12.tenkir@gmail.com>
2021-06-17 22:13:33 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * The function prints out a string
+ *
+ * Return: Always 0 (Success)
+ */
+int mai(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
