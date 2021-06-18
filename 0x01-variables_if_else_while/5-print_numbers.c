09ba116e049c440289cc34bfcadbe0ea0b962b1c
Milli-saved <million12.tenkir@gmail.com>
2021-06-18 01:45:21 -0700

    Fifth question answer

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * This function prints one digit numbers.
+ */
+int main(){
+
+for(int x=0;x<10;x++){
+printf("%d",x);
+}
+printf("\n");
+  
+return 0;
+}
