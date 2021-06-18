0e17d8365dcba3743cbbef341a6569977e5799eb
Milli-saved <million12.tenkir@gmail.com>
2021-06-18 01:19:44 -0700

    Done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * The function prints out a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(){
+
+for(int x = 'A'; x<= 'Z'; x++){
+int lower_case = tolower(x);
+putchar(lower_case);
+}
+  
+return 0;
+}
