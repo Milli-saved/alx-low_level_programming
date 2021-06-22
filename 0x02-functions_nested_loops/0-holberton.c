57c924f69528f4ae8728605fe92941adbd303359
Milli-saved <million12.tenkir@gmail.com>
2021-06-22 10:32:57 -0700

    Done

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,24 +2,21 @@
 #include "holberton.h"
 
 /**
- *
- * main - Entery
- *
+ * main- Entery
  * Return : always 0(success"
- *
  * This program prints Holberton
  */
 
 int main(void){
-  _putchar('H');
-  _putchar('o');
-  _putchar('l');
-  _putchar('b');
-  _putchar('e');
-  _putchar('r');
-  _putchar('t');
-  _putchar('o');
-  _putchar('n');
-  _putchar('\n');
-  return (0);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
 }
