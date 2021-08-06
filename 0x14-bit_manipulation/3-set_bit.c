3515f6915d4627393994740a57bf99fbffe26975
Milli-saved <million12.tenkir@gmail.com>
2021-08-06 13:51:44 -0700

    fix trial

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * set_bit - gets bit on index `index`
- * @n: decimal number
- * @index: index of the bit
- * Return: 1 (success) or -1(faillure)
+ * set_bit - sets the value of a bit to 1.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
  */
-
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= (sizeof(unsigned long int) * BYTE_LENGHT))
+unsigned int m;
+
+if (index > 63)
 return (-1);
 
-*n ^= (1 << index);
+m = 1 << index;
+*n = (*n | m);
 
 return (1);
 }
