2b5a687417dd41e6f79f614d375b131632ea444c
Milli-saved <million12.tenkir@gmail.com>
2021-08-10 06:41:02 -0700

    late fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,20 +9,20 @@
  */
 int wrdcnt(char *s)
 {
-	int i, n = 0;
+int i, n = 0;
 
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] == ' ')
-		{
-			if (s[i + 1] != ' ' && s[i + 1] != '\0')
-				n++;
-		}
-		else if (i == 0)
-			n++;
-	}
-	n++;
-	return (n);
+for (i = 0; s[i]; i++)
+{
+if (s[i] == ' ')
+{
+if (s[i + 1] != ' ' && s[i + 1] != '\0')
+n++;
+}
+else if (i == 0)
+n++;
+}
+n++;
+return (n);
 }
 
 /**
